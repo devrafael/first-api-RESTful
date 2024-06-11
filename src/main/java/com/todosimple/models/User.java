7d8d84bfb4bae01913cf65a7d3aa89d597d9d67c
devrafael <almeida9137@gmail.com>
2024-06-11 15:58:50 -0300

    Project Configuration, Create User and Task entities

---
+++ src/main/java/com/todosimple/models/User.java
@@ -0,0 +1,47 @@
+package com.todosimple.models;
+
+
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonProperty.Access;
+
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = User.TABLE_NAME)
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class User {
+
+    public static final String TABLE_NAME = "user";
+
+    @Id
+    @Column(name = "id", unique = true)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "username", length = 100, nullable = false, unique = true)
+    @Size(min = 2, max = 100)
+    @NotBlank
+    private String username;
+
+    @Column(name = "password", length = 60, nullable = false)
+    @JsonProperty(access = Access.WRITE_ONLY)
+    @Size(min = 8, max = 60)
+    @NotBlank
+    private String password;
+
+    @OneToMany(mappedBy = "user")
+    @JsonProperty(access = Access.WRITE_ONLY)
+    private List<Task> tasks = new ArrayList<Task>();
+}
